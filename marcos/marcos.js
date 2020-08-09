29639d60f85ff633d5d1e9d4ef67b744122db87a
marco p <miamarine84@yahoo.com>
2020-08-09 18:23:10 -0400

    some html was changed and started working on the JS as well as the AJAX

--- marcos/marcos.js
+++ marcos/marcos.js
@@ -1,4 +1,30 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function() {
+    console.log("ready!");
+
+
+   $('#checkVin') .on('click', function(){
+       console.log('it worked')
+    //    $('#carouselExampleSlidesOnly').html('')
+       $('.vin-compare').css('display', 'block' )
+   })
+
+   function userInputVin (){
+
+   let userVinNumber = $('#vinNumber').val()
+   let objecVin = 'https://specifications.vinaudit.com/v3/specifications?key=0UCAOK5F1GEGDMD&format=xml&include=attributes,equipment,colors,recalls,warranties,photos&vin=' + userVinNumber;
+
+
+   $.ajax({
+    url: objecVin,
+    type: "GET"
+  }).then(function(response){
+      response
+  });
+   }
+
+
+
+
+
 });
 
